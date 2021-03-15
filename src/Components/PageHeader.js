ec8e27ae629fd5a833f0ae6a92fa8a18a277c05a
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-03-15 16:58:50 -0700

    imported Link into PageHeader and replaced the placeholder anchors with Links

--- src/Components/PageHeader.js
+++ src/Components/PageHeader.js
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
+
 const PageHeader = () => {
   return (
     <header>
-    <img alt="Logo" src={null}></img>
+      <img alt="Logo" src={null}></img>
       <nav>
-          <a href='/contact'>Contact</a>
-          <a href='/terms'>Sign In</a>
-          <a href='/cart'>Cart</a>
+        <Link to="/profile">Sign In</Link>
+        <Link to="/cart">Cart</Link>
       </nav>
     </header>
   );
