b70aeca4c64fc0cbe343df84da17a11f17209dd7
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-03-15 17:48:51 -0700

    made ShopPage and added as route and linked to homepage

---
+++ src/Components/ShopPage.js
@@ -0,0 +1,13 @@
+const ShopPage = () => {
+    return (
+        <main>
+            <h1>Shop</h1>
+            <h2>Section 1</h2>
+            <h2>Section 2</h2>
+            <h2>Section 3</h2>
+            <h2>Section 4</h2>
+        </main>
+    )
+}
+
+export default ShopPage;
--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./App";
 import HomePage from "./Components/HomePage";
+import ShopPage from './Components/ShopPage';
 import Cart from "./Components/CartPage";
 import Profile from "./Components/Profle";
 import PageHeader from "./Components/PageHeader";
@@ -14,6 +15,7 @@ const Routes = () => {
         <Route path="/cart" component={Cart} />
         <Route path="/contact" />
         <Route path="/profile" component={Profile} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </BrowserRouter>
   );
