323713fdf8aa674704812c96f4350e939b3bc1c4
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-03-15 19:19:19 -0700

    mapped through the different category of items in stock to list items in ShopPage component

--- src/Components/ShopPage.js
+++ src/Components/ShopPage.js
@@ -1,21 +1,36 @@
-import ItemListing from './ItemListing';
-import stock from '../items/stock';
+import ItemListing from "./ItemListing";
+import stock from "../items/stock";
 
 const ShopPage = () => {
-    return (
-        <main>
-            <h1>Shop</h1>
-            <h2>Section 1</h2>
-            <div>
-            <h2>Succulents</h2>
-            {stock.succulents.map(item => (
-                <ItemListing key={item.id} item={item}/>
-            ))}
-            </div>
-            <h2>Section 3</h2>
-            <h2>Section 4</h2>
-        </main>
-    )
-}
+  return (
+    <main>
+      <h1>Shop</h1>
+      <div>
+        <h2>Houseplants</h2>
+        {stock.houseplants.map((item) => (
+          <ItemListing key={item.id} item={item} />
+        ))}
+      </div>
+      <div>
+        <h2>Succulents</h2>
+        {stock.succulents.map((item) => (
+          <ItemListing key={item.id} item={item} />
+        ))}
+      </div>
+      <div>
+        <h2>Tillandsia (Air Plants)</h2>
+        {stock.tillandsia.map((item) => (
+          <ItemListing key={item.id} item={item} />
+        ))}
+      </div>
+      <div>
+        <h2>Section 4</h2>
+        {stock.supplies.map((item) => (
+          <ItemListing key={item.id} item={item} />
+        ))}
+      </div>
+    </main>
+  );
+};
 
 export default ShopPage;
