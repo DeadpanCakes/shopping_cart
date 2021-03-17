c40a3cc17e5c4c273bc47e5f15b954e13c86193f
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-03-17 13:14:44 -0700

    made handleIncrement and handleDecrement fn and assigned to buttons

--- src/Components/ShopPages/ItemPage.js
+++ src/Components/ShopPages/ItemPage.js
@@ -1,15 +1,25 @@
 import { Fragment, useState } from "react";
-import stock from '../../items/stock';
+import stock from "../../items/stock";
 const ItemPage = (props) => {
   const { id } = props.match.params;
 
   const fetchItem = (productId) => {
-    return stock.items.find((product) => product.id === productId)
-  }
+    return stock.items.find((product) => product.id === productId);
+  };
 
-  const item = fetchItem(id)
+  const item = fetchItem(id);
 
   const [count, setCount] = useState(1);
+  const handleIncrement = (e) => {
+    e.preventDefault();
+    setCount((prevCount) => prevCount + 1);
+  };
+  const handleDecrement = (e) => {
+    e.preventDefault();
+    if (count > 1) {
+      setCount((prevCount) => prevCount - 1);
+    }
+  };
   const handleInput = (value) => {
     if (typeof Number(value) === "number" && !isNaN(Number(value))) {
       setCount(value);
@@ -17,10 +27,9 @@ const ItemPage = (props) => {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newItem= {...item, quantity: e.target.quantity.value}
-    props.addToCart(newItem)
-    console.log(props.cart)
-  }
+    const newItem = { ...item, quantity: e.target.quantity.value };
+    props.addToCart(newItem);
+  };
 
   return (
     <Fragment>
@@ -29,9 +38,13 @@ const ItemPage = (props) => {
         <h1>{item.name}</h1>
         <h2>${item.price}.00</h2>
         <form onSubmit={handleSubmit}>
-          <button onClick={() => setCount(count - 1)}>-</button>
-          <input value={count} onChange={(e) => handleInput(e.target.value)} name='quantity' />
-          <button onClick={() => setCount(count + 1)}>+</button>
+          <button onClick={handleDecrement}>-</button>
+          <input
+            value={count}
+            onChange={(e) => handleInput(e.target.value)}
+            name="quantity"
+          />
+          <button onClick={handleIncrement}>+</button>
           <button>Add To Cart</button>
         </form>
       </div>
