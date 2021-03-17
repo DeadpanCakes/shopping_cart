f45664641a32218a33e6622f780eeda80d3ad3db
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-03-17 15:04:52 -0700

    added check to see if cart is empty when rendering CartPage

--- src/Components/CartPage.js
+++ src/Components/CartPage.js
@@ -1,8 +1,31 @@
+import { Fragment } from "react";
+import ItemListing from "./ItemListing";
+
 const CartPage = (props) => {
+  const { cart } = props;
   return (
     <div>
       <h1>Shopping Cart</h1>
-      <p>{props.cart[0]}</p>
+      {cart[0] ? (
+        <Fragment>
+          {props.cart.map((item) => (
+            <Fragment>
+              <p>${item.price * item.quantity}</p>
+              <ItemListing item={item} />
+              <p>{item.quantity}</p>
+            </Fragment>
+          ))}
+          <h2>
+            Total:{" "}
+            {cart.reduce(
+              (total, item) =>
+                total + Number(item.price) * Number(item.quantity)
+            )}
+          </h2>
+        </Fragment>
+      ) : (
+        <p>Your Cart Is Empty</p>
+      )}
     </div>
   );
 };
