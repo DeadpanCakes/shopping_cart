d57d8084f8c329a3f6ae17ee1e3861c899cfd689
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-03-18 11:57:04 -0700

    added logic to CheckOutPage to display order total

--- src/Components/CheckOutPage.js
+++ src/Components/CheckOutPage.js
@@ -1,11 +1,24 @@
 import { Fragment } from "react";
 
 const CheckoutPage = (props) => {
-    const total = props.getwithtax
+  const { cart } = props;
+  const totalCost = (() => {
+      const priceArr = cart.map(item=> item.price*item.quantity)
+      const subTotal = priceArr.reduce((total, item) => total+item)
+      const tax = subTotal / 10
+      const shipping = 8
+      const formatter = Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: "USD",
+      })
+      return formatter.format(subTotal + tax + shipping)
+  })()
   return (
     <Fragment>
       <main></main>
-      <aside><h1>Total: {total}</h1></aside>
+      <aside>
+        <h1>Total: {totalCost}</h1>
+      </aside>
     </Fragment>
   );
 };
