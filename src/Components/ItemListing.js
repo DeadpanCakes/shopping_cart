f4baeddf186d910f4a593f01d12e379092f5d876
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-03-23 14:53:08 -0700

    wrapped name and price in ItemListing in container to better structure using flex

--- src/Components/ItemListing.js
+++ src/Components/ItemListing.js
@@ -6,14 +6,16 @@ const ItemListing = (props) => {
   return (
     <li key={item.key} className="itemListing">
       <Link to={`/shop/item/${item.id}`}>
-        <div>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
           <img
             alt={`${item.name}`}
             src={item.imgSrc}
-            style={{ maxWidth: "70%"}}
+            style={{ maxWidth: "70%", borderRadius:'20px', marginBottom: '20px' }}
           />
-          <h2>{item.name}</h2>
-          <h4>${item.price}</h4>
+          <div style={{display:'flex', marginBottom: '10px'}}>
+            <h2>{item.name} -</h2>
+            <h4>${item.price}</h4>
+          </div>
         </div>
       </Link>
     </li>
