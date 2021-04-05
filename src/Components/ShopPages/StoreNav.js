3171e4d737c996d55d3f5a8aceed08eeeea8e5cb
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-04-05 13:45:44 -0700

    animated arrow next to storeNavButton

--- src/Components/ShopPages/StoreNav.js
+++ src/Components/ShopPages/StoreNav.js
@@ -1,47 +1,63 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const StoreNav = () => {
+  const [expanded, setExpanded] = useState(false);
+
   const toggleNav = () => {
-    console.log('toggled')
+    setExpanded((prevState) => (prevState ? false : true));
+  };
+
+  const navLinkStyle = {
+    padding: '5px',
+    width: '100%',
+    backgroundColor: 'white',
+    border: '1px solid gray'
   }
 
   return (
-    <Fragment>
-      <Link to="/shop">Go Back</Link>
-      <button onClick={toggleNav}>
-        <h2>Navigation</h2>
-      </button>
-      <nav>
-        <ul>
-          <Link to="/shop/allplants">
-            <li>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+    <button className={expanded ? 'storeNavButton expandedStoreNavButton' : 'storeNavButton collapsedStoreNavButton'} onClick={toggleNav}>
+      <h2>Navigation</h2>
+    </button>
+      <nav
+        className={expanded ? "expandedStoreNav storeNav" : "storeNav"}
+        style= {expanded ? {maxHeight:'200px'} : {maxHeight: '0'}}
+      >
+        <ul id="shopNavList">
+          <li style={navLinkStyle}>
+            <Link to="/shop">Shop Home</Link>
+          </li>
+          <li style={navLinkStyle}>
+            <Link to="/shop/allplants">
               <h2>All Plants</h2>
-            </li>
-          </Link>
-          <Link to="/shop/houseplants">
-            <li>
+            </Link>
+          </li>
+          <li style={navLinkStyle}>
+            <Link to="/shop/houseplants">
               <h2>Housplants</h2>
-            </li>
-          </Link>
-          <Link to="/shop/succulents">
-            <li>
+            </Link>
+          </li>
+          <li style={navLinkStyle}>
+            <Link to="/shop/succulents">
               <h2>Succulents</h2>
-            </li>
-          </Link>
-          <Link to="/shop/tillandsia">
-            <li>
+            </Link>
+          </li>
+          <li style={navLinkStyle}>
+            <Link to="/shop/tillandsia">
               <h2>Tillandsia</h2>
-            </li>
-          </Link>
-          <Link to="/shop/supplies">
-            <li>
+            </Link>
+          </li>
+          <li style={navLinkStyle}>
+            <Link to="/shop/supplies">
               <h2>Supplies</h2>
-            </li>
-          </Link>
+            </Link>
+          </li>
         </ul>
       </nav>
-    </Fragment>
+    </div>
   );
 };
 
