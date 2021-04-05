6a446d9f0ab34cc2c589b2c8389b96b4bd5b9428
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-04-05 15:12:58 -0700

    styled socialMediaLinks to glow when hovered over

--- src/Components/Footer/SocialMedia.js
+++ src/Components/Footer/SocialMedia.js
@@ -8,26 +8,21 @@ const SocialMedia = () => {
     alignItems: 'center'
   }
 
-  const aStyle = {
-    color: '#f6c7c7',
-    padding: '0 10px',
-  }
-
   return (
     <nav>
       <ul style={containerStyle}>
         <li>
-          <a href="https://twitter.com/" style={aStyle}>
+          <a href="https://twitter.com/" className='socialMediaLink'>
             <FontAwesomeIcon icon={faTwitter}  size={'lg'}/>
           </a>
         </li>
         <li>
-          <a href="https://instagram.com/" style={aStyle}>
+          <a href="https://instagram.com/" className='socialMediaLink'>
             <FontAwesomeIcon icon={faInstagram} size={'lg'}/>
           </a>
         </li>
         <li>
-          <a href="https://facebook.com/" style={aStyle}>
+          <a href="https://facebook.com/" className='socialMediaLink'>
             <FontAwesomeIcon icon={faFacebook}  size={'lg'}/>
           </a>
         </li>
