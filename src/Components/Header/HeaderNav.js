05c1c0efc3cebf32aff33bdb3f121dea55a2b3e7
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-04-06 15:27:54 -0700

    moved remaining code in PageHeader into own component called HeaderNav

--- src/Components/Header/HeaderNav.js
+++ src/Components/Header/HeaderNav.js
@@ -1,8 +1,51 @@
-const HeaderNav = () => {
-    return (
-        <nav>
-        </nav>
-    )
-}
+import { Link } from "react-router-dom";
+import logo from "../../img/headerLogo.png";
+import { faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default HeaderNav
+const HeaderNav = ( props ) => {
+
+    const {cart} = props
+  
+    const navContainerStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    minHeight: "10vh",
+  };
+
+  return (
+    <nav style={navContainerStyle}>
+      <ul
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          minHeight: "100%",
+          minWidth: "100vw",
+        }}
+      >
+        <li>
+          <Link to="/" style={{ width: "5%" }}>
+            <img alt="Logo" src={logo} style={{ maxHeight: "100%" }}></img>
+          </Link>
+        </li>
+        <li
+          style={{ display: "flex", alignItems: "center", marginRight: "20px" }}
+        >
+          <Link to="/profile" className="headerNav">
+            <FontAwesomeIcon icon={faUser} />
+          </Link>
+          <Link to="/cart" className="headerNav" id="headerCart">
+            <FontAwesomeIcon icon={faShoppingCart} />
+            {cart.length > 0 ? (
+              <p id="cartCount" style={{ fontSize: "1em" }}>
+                {cart.length}
+              </p>
+            ) : null}
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default HeaderNav;
--- src/Components/Header/PageHeader.js
+++ src/Components/Header/PageHeader.js
@@ -1,8 +1,5 @@
-import { Link } from "react-router-dom";
 import TopBanner from "./TopBanner";
-import logo from "../../img/headerLogo.png";
-import { faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import HeaderNav from  './HeaderNav';
 
 const PageHeader = (props) => {
   const containerStyle = {
@@ -24,38 +21,11 @@ const PageHeader = (props) => {
     zIndex: "1",
   };
 
-  const navContainerStyle = {
-    display: "flex",
-    justifyContent: "space-between",
-    minHeight: '10vh'
-  };
-
   return (
     <div className="headerContainer" style={containerStyle}>
       <header style={headerStyle}>
         <TopBanner />
-        <nav style={navContainerStyle}>
-          <ul style={{display: 'flex', justifyContent: 'space-between', minHeight: '100%', minWidth: '100vw'}}>
-            <li>
-            <Link to="/" style={{ width: "5%" }}>
-              <img alt="Logo" src={logo} style={{ maxHeight: "100%" }}></img>
-            </Link>
-            </li>
-            <li style={{display: 'flex', alignItems: 'center', marginRight: '20px',}}>
-              <Link to="/profile" className="headerNav">
-                <FontAwesomeIcon icon={faUser} />
-              </Link>
-              <Link to="/cart" className="headerNav" id="headerCart">
-                <FontAwesomeIcon icon={faShoppingCart} />
-                {props.cart.length > 0 ? (
-                  <p id="cartCount" style={{ fontSize: "1em" }}>
-                    {props.cart.length}
-                  </p>
-                ) : null}
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <HeaderNav cart={props.cart} />
       </header>
     </div>
   );
