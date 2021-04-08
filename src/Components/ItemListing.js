b2601be5f2afc29c6bbece510322712fde830b6b
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-04-08 15:32:47 -0700

    made limitChar fn in ItemListing to limit length of name where necessary

--- src/Components/ItemListing.js
+++ src/Components/ItemListing.js
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 const ItemListing = (props) => {
   const { item } = props;
 
+  //Limit string length unless hovered over
+  const limitChar = (str) => {
+    if (str.length > 10) {
+      return str.slice(0,10) + '...'
+    }
+    return str
+  }
+
   return (
     <li key={item.key} className="itemListing">
       <Link to={`/shop/item/${item.id}`}>
@@ -13,7 +21,7 @@ const ItemListing = (props) => {
             style={{ maxWidth: "70%", borderRadius:'20px', marginBottom: '20px' }}
           />
           <div style={{display:'flex', marginBottom: '10px'}}>
-            <h2>{item.name} -</h2>
+            <h2>{limitChar(item.name)}</h2>
             <h4>${item.price}</h4>
           </div>
         </div>
