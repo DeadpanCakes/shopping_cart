1afac79e838ee07aa61eb8930b6d389698474d86
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-04-15 17:43:43 -0700

    replaced placeholder img in ShopHome with higher res imgs

--- src/Components/ShopPages/PageSection.js
+++ src/Components/ShopPages/PageSection.js
@@ -1,17 +1,25 @@
-import {Link} from 'react-router-dom';
-import ItemListing from '../ItemListing';
+import { Link } from "react-router-dom";
+import ItemListing from "../ItemListing";
 
 const PageSection = (props) => {
   const { name, category, link, bannerImg } = props;
 
   const categoryStyle = {
     display: "flex",
-    flexWrap: "nowrap",
   };
 
   const Banner = (props) => {
-    return (<img src={props.src} alt={props.alt}></img>)
-  }
+    return (
+      <Link
+        style={{ position: "relative", width: "100%", alignSelf: "flex-end", color: 'white' }}
+        to={link}
+        className='banner'
+      >
+        <img src={props.src} alt={props.alt}></img>
+        <h2 className='bannerText'>{`See All ${name}`}</h2>
+      </Link>
+    );
+  };
 
   return (
     <div className="shopSections">
@@ -21,13 +29,7 @@ const PageSection = (props) => {
           <ItemListing key={item.id} item={item} />
         ))}
       </div>
-      <Link
-        style={{ width: '100%', alignSelf: "flex-end" }}
-        to={link}
-      >
-          <Banner src={bannerImg} alt='test' />
-          {`See All ${name}`}
-          </Link>
+      <Banner src={bannerImg} alt="test" />
     </div>
   );
 };
