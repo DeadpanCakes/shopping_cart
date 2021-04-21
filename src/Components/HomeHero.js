e78e04dd15e0471c35ef5111c0cc6748d6548654
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-04-20 17:14:05 -0700

    moved Link to '/shop' into HomeHero and applied to button

--- src/Components/HomeHero.js
+++ src/Components/HomeHero.js
@@ -1,15 +1,18 @@
-import heroImg from "../img/homeHero.png"
+import heroImg from "../img/homeHero.png";
+import { Link } from "react-router-dom";
 
 const HomeHero = () => {
   const containerStyle = {
-      backgroundImage: `url(${heroImg})`,
-      minHeight: '100vh'
+    backgroundImage: `url(${heroImg})`,
+    minHeight: "100vh",
   };
 
   return (
     <div style={containerStyle}>
       <h1>Houseplant Hero</h1>
-      <button>Bring Life Into Your Home</button>
+      <Link to="/shop">
+        <button>Bring Life Into Your Home</button>
+      </Link>
     </div>
   );
 };
