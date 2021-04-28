42277accd1990b9b79c118a23911fba89607eeb4
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-04-28 16:41:49 -0700

    added styling to CheckoutListing

--- src/Components/Checkout/CheckoutListing.js
+++ src/Components/Checkout/CheckoutListing.js
@@ -2,7 +2,7 @@ const CheckoutListing = (props) => {
   const { item } = props;
 
   return (
-    <li>
+    <li style={{display: 'flex', height: '70px'}}>
       <img src={item.imgSrc} alt={`A(n) ${item.name}`}></img>
       <h3>{`${item.quantity}x ${item.name}`}</h3>
       <h2>{`$${item.quantity * item.price}`}</h2>
