0c279fe4b0ae06db72fdefcea8487de26cb25cc9
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-04-28 14:48:22 -0700

    moved keyboard listener logic into onKeyPress instead of useEffect

--- src/Components/Checkout/CheckoutForm.js
+++ src/Components/Checkout/CheckoutForm.js
@@ -3,10 +3,33 @@ import orderFactory from "../../orderFactory";
 import checkoutSteps from "./checkoutSteps";
 
 const CheckoutForm = (props) => {
-  const [signUpInfo, setSignUpInfo] = useState({email: '', pass: '', verifyPass: ''})
-  const [shippingInfo, setShippingInfo] = useState({name: '',country: '', address: '', zip: '', city: '', phone: ''})
-  const [billingInfo, setBillingInfo] = useState({name: '',country: '', address: '', zip: '', city: '', phone: ''})
-  const [paymentInfo, setPaymentInfo] = useState({cardNumber: '', name: '', expire: '', code: ''})
+  const [signUpInfo, setSignUpInfo] = useState({
+    email: "",
+    pass: "",
+    verifyPass: "",
+  });
+  const [shippingInfo, setShippingInfo] = useState({
+    name: "",
+    country: "",
+    address: "",
+    zip: "",
+    city: "",
+    phone: "",
+  });
+  const [billingInfo, setBillingInfo] = useState({
+    name: "",
+    country: "",
+    address: "",
+    zip: "",
+    city: "",
+    phone: "",
+  });
+  const [paymentInfo, setPaymentInfo] = useState({
+    cardNumber: "",
+    name: "",
+    expire: "",
+    code: "",
+  });
   const [checkoutStep, setCheckoutStep] = useState(1);
   const incrementStep = () => {
     if (checkoutStep < 4) {
@@ -26,49 +49,91 @@ const CheckoutForm = (props) => {
     PaymentSection,
   ] = checkoutSteps;
 
-  useEffect(() => {
-    const form = document.getElementById('checkoutForm');
-    const listener = (event) => {
-      if (event.code === 'Enter' || event.code=== 'NumpadEnter') {
-        incrementStep();
-      }
-    }
-    document.addEventListener('keydown', listener);
-    return () => document.removeEventListener('keydown',listener);
-  },[])
+  // useEffect(() => {
+  //   const listener = (event) => {
+  //     if (event.code === "Enter" || event.code === "NumpadEnter") {
+  //       if (checkoutStep > 4) {
+  //         incrementStep();
+  //       } else {
+  //         completeTransaction();
+  //       }
+  //     }
+  //   };
+  //   document.addEventListener("keydown", listener);
+  //   return () => document.removeEventListener("keydown", listener);
+  // }, []);
+
+
 
   const fetchStep = (step) => {
     switch (step) {
       case 2:
-        return <ShippingSection shippingInfo={shippingInfo} setShippingInfo={setShippingInfo}/>;
+        return (
+          <ShippingSection
+            shippingInfo={shippingInfo}
+            setShippingInfo={setShippingInfo}
+          />
+        );
       case 3:
-        return <BillingSection billingInfo={billingInfo} setBillingInfo={setBillingInfo} />;
+        return (
+          <BillingSection
+            billingInfo={billingInfo}
+            setBillingInfo={setBillingInfo}
+          />
+        );
       case 4:
-        return <PaymentSection paymentInfo={paymentInfo} setPaymentInfo={setPaymentInfo}/>;
+        return (
+          <PaymentSection
+            paymentInfo={paymentInfo}
+            setPaymentInfo={setPaymentInfo}
+          />
+        );
       default:
-        return <SignUpSection signUpInfo={signUpInfo} setSignUpInfo={setSignUpInfo} />;
+        return (
+          <SignUpSection
+            signUpInfo={signUpInfo}
+            setSignUpInfo={setSignUpInfo}
+          />
+        );
     }
   };
 
   const completeTransaction = () => {
-    //Generate order using orderFactory 
-    props.emptyCart()
-  }
+    //Generate order using orderFactory
+    const order = orderFactory(
+      props.items,
+      new Date(),
+      shippingInfo,
+      billingInfo,
+      paymentInfo
+    );
+    console.log(order);
+    props.emptyCart();
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('submitreached')
-    if (checkoutStep < 4 ) {
-      console.log('test.pcrh')
-      incrementStep()
+    console.log("submitreached");
+    if (checkoutStep < 4) {
+      console.log("test.pcrh");
+      incrementStep();
+    } else {
+      completeTransaction();
     }
-    else {
-      completeTransaction()
+  }; 
+
+  const handleKeyPress = (e) => {
+    if (e.code === "Enter" || e.code === "NumpadEnter") {
+      if (checkoutStep < 4) {
+        incrementStep();
+      } else {
+        completeTransaction();
+      }
     }
   }
 
   return (
-    <form onSubmit={(e) => handleSubmit(e)} id='checkoutForm'>
+    <form onSubmit={(e) => handleSubmit(e)} onKeyPress={handleKeyPress} id="checkoutForm">
       <p>{checkoutStep}</p>
       {fetchStep(checkoutStep)}
       <button
