f6769a8ae3a771ea958063c924086154722559bf
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-04-29 16:58:40 -0700

    fixed error with styling that caused overflow when items with long names were added to cart

--- src/Components/Checkout/CheckoutPage.js
+++ src/Components/Checkout/CheckoutPage.js
@@ -45,7 +45,7 @@ const CheckoutPage = () => {
             }}
           >
             <CheckoutForm emptyCart={emptyCart} />
-            <aside style={{ flexGrow: 1, flexBasis: 1 }}>
+            <aside style={{ flexShrink: 1, maxWidth:"50%" }}>
               <ul>
                 {cart.items.map((item) => {
                   return <CheckoutListing key={item.id} item={item} />;
