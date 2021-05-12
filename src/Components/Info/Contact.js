829b153ba69fcee0601c6d81374884b4f577014b
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-05-11 18:55:30 -0700

    added a 'Make An Appointment' section in Contact

--- src/Components/Info/Contact.js
+++ src/Components/Info/Contact.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
+
 const Contact = () => {
   const mainStyle = {
     height: "100vh",
@@ -11,7 +14,7 @@ const Contact = () => {
         <p>
           Got questions or feedback? Or maybe you just wanna say thanks? If you
           want a response, remember to leave your email. If you need quick
-          answers, be sure to check our FAQ
+          answers, be sure to check our FAQ.
         </p>
         <label>
           Email<input></input>
@@ -22,6 +25,15 @@ const Contact = () => {
         <label>
           <textarea></textarea>
         </label>
+        <h2>Make An Appointment</h2>
+        <p>
+          You can visit us and pick out your own plants or find specimens not
+          listed on the site. Just give us a ring to set up an appointment!
+        </p>
+        <FontAwesomeIcon icon={faPhone}></FontAwesomeIcon>
+        <p>+1 (883) 841-2369</p>
+        <FontAwesomeIcon icon={faMapMarkedAlt}></FontAwesomeIcon>
+        <p>475 Konopelski Harbors, San Diego, Ca 94516</p>
         <h2>Find Us At:</h2>
         <ul>
           <li>Twitter</li>
