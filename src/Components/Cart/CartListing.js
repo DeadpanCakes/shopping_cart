19e520a72ead2de4c687136cefa35e9e6200d1e2
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-05-25 16:22:12 -0700

    added Link to CartListing

--- src/Components/Cart/CartListing.js
+++ src/Components/Cart/CartListing.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const CartListing = (props) => {
   const {
     item,
@@ -9,20 +11,33 @@ const CartListing = (props) => {
   const { id, name, price, quantity } = item;
 
   const imgStyle = { maxHeight: 100, maxWidth: 100 };
-  const containerStyle = { display: "flex", justifyContent: 'space-between', alignItems: "center", width: '100%', margin: 5 };
-  const inputStyle = { width: 50 }
-  const controlStyle = {marginRight: 50}
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    margin: 5,
+  };
+  const inputStyle = { width: 50 };
+  const controlStyle = { marginRight: 50 };
   return (
     <li style={containerStyle}>
-      <img alt={name} src={item.imgSrc} style={imgStyle} />
-      <h2 style={{flex: 1}}>{name}</h2>
+      <Link to={`/shop/item/${id}`} style={{flex: 1}} className="itemLink">
+        <img alt={name} src={item.imgSrc} style={imgStyle} />
+        <h2>{name}</h2>
+      </Link>
       <form name={id} onSubmit={(e) => e.preventDefault()} style={controlStyle}>
         <button onClick={decrementQuantity}>-</button>
-        <input name="quantity" value={item.quantity} onChange={handleInput} style={inputStyle}/>
+        <input
+          name="quantity"
+          value={item.quantity}
+          onChange={handleInput}
+          style={inputStyle}
+        />
         <button onClick={incrementQuantity}>+</button>
         <button onClick={() => removeItem(item)}>Remove</button>
       </form>
-      <div style={{ display: 'flex', flexDirection: "column"}}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
         <p>Price: ${price}</p>
         <p>Total: ${price * quantity}</p>
       </div>
