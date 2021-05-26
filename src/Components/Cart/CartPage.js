1e32546586418d6c1cdd16e8a0b6b3d324dcf065
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-05-26 16:35:45 -0700

    added comment box to cart

--- src/Components/Cart/CartPage.js
+++ src/Components/Cart/CartPage.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { CartConsumer } from "../../CartContext";
 import RecommendedSection from "../RecommendedSection";
 import CartListing from "./CartListing";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const CartPage = () => (
   <CartConsumer>
@@ -41,11 +43,28 @@ const CartPage = () => (
 
       return (
         <div>
-          <h1 style={{width: '100vw', color: 'white', backgroundColor: "#f6c7c7", padding: '10px 0', paddingLeft: '30px'}}>Shopping Cart</h1>
-          <div style={{ display: 'flex', flexDirection:'column', width: "60vw", marginLeft: "20vw" }}>
-            <div style={{display: 'flex'}}>
+          <h1
+            style={{
+              width: "100vw",
+              color: "white",
+              backgroundColor: "#f6c7c7",
+              padding: "10px 0",
+              paddingLeft: "30px",
+            }}
+          >
+            Shopping Cart
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "60vw",
+              marginLeft: "20vw",
+            }}
+          >
+            <div style={{ display: "flex" }}>
               <h2>Name</h2>
-              <h2 style={{marginLeft: 'auto', marginRight: 100}}>Count</h2>
+              <h2 style={{ marginLeft: "auto", marginRight: 100 }}>Count</h2>
               <h2>Price</h2>
             </div>
             {items.length > 0 ? (
@@ -62,17 +81,37 @@ const CartPage = () => (
                     />
                   ))}
                 </ul>
-                <div style={{ alignSelf: "flex-end" }}>
-                  <h2>
-                    Total:$
-                    {items
-                      .map((item) => item.price * item.quantity)
-                      .reduce((total, price) => total + price)}
-                    .00
-                  </h2>
-                  <Link to="/cart/checkout">
-                    <button>Check Out</button>
-                  </Link>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <label>Notes</label>
+                    <textarea
+                      placeholder="Feel free to leave any notes you feel are relevant to your order and we will do our best to accomodate!"
+                      style={{ width: 200, height: 100, resize: 'vertical'}}
+                    ></textarea>
+                    <Link
+                      to="/contact"
+                      style={{ width: 200, fontSize: ".9em" }}
+                      id="cartComment"
+                    >
+                      <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon> Have
+                      a queston you need answered before committing to an order?
+                      Send us a message!
+                    </Link>
+                  </div>
+                  <div>
+                    <h2>
+                      Total:$
+                      {items
+                        .map((item) => item.price * item.quantity)
+                        .reduce((total, price) => total + price)}
+                      .00
+                    </h2>
+                    <Link to="/cart/checkout">
+                      <button>Check Out</button>
+                    </Link>
+                  </div>
                 </div>
               </>
             ) : (
