257174ed289f24ddc39170389c2138f26bc252ec
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-06-07 17:39:47 -0700

    fixed issue with ItemPage causing unexpected overflow

--- src/Components/ShopPages/ItemPage.js
+++ src/Components/ShopPages/ItemPage.js
@@ -7,7 +7,7 @@ import { CartConsumer } from "../../CartContext";
 const ItemPage = (props) => {
   const { id } = props.match.params;
   const [item, setItem] = useState({});
-  const itemArr = [item]
+  const itemArr = [item];
 
   useEffect(() => {
     const fetchItem = (productId) => {
@@ -20,7 +20,7 @@ const ItemPage = (props) => {
   return (
     <CartConsumer>
       {(cart) => {
-        const {items, addItem} = cart
+        const { items, addItem } = cart;
 
         return (
           <div className="itemPageContainer">
@@ -31,13 +31,11 @@ const ItemPage = (props) => {
                 className="itemPageImg"
               ></img>
               <div className="itemInfo">
-                <h1>{item.name}</h1>
-                <h2>${item.price}.00</h2>
-                <ItemControls
-                  item={item}
-                  cart={items}
-                  addToCart={addItem}
-                />
+                <div>
+                  <h1>{item.name}</h1>
+                  <h2>${item.price}.00</h2>
+                </div>
+                <ItemControls item={item} cart={items} addToCart={addItem} />
               </div>
             </main>
             <RecommendedSection itemArr={itemArr} />
