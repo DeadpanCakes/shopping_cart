85ba0298aaab5e1fc6aad15fb75a8db9273f64cc
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-06-08 19:09:17 -0700

    styled About page

--- src/Components/Info/About.js
+++ src/Components/Info/About.js
@@ -1,41 +1,139 @@
 import { Link } from "react-router-dom";
+import heroImg from "../../img/aboutBannerFM.jpg";
+import storyImg from "../../img/aboutImg1.webp";
+import missionImg from "../../img/aboutImg2.webp";
+import RecommendedSection from '../RecommendedSection';
 
 const About = () => {
-  const mainStyle = { height: "100vh" };
+  const containerStyle = {
+    minHeight: "100vh",
+  };
+
+  const mainStyle = {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    marginTop: 40,
+  };
+
+  const bannerStyle = {
+    width: "100%",
+    maxHeight: 400,
+    minHeight: 400,
+    position: "relative",
+    backgroundImage: `url(${heroImg})`,
+  };
+
+  const heroText = {
+    position: "absolute",
+    left: "6%",
+    bottom: "8%",
+    color: "white",
+  };
+
+  const sectionStyle = {
+    width: "86vw",
+    marginLeft: "7vw",
+    padding: "40px",
+  };
+
+  const breakStyle = {
+    border: "1px solid black",
+    width: "80%",
+  };
+
+  const titleStyle = {
+    textAlign: "center",
+  };
+
+  const textStyle = {
+    fontSize: "1.3em",
+  };
 
   return (
-    <main style={mainStyle}>
-      <h1>Our Story</h1>
-      <p>
-        Mollitia illo voluptatem tempore voluptatem. Molestiae dolorem nostrum
-        cum. Ut unde explicabo totam id consequuntur quae non. Est sit sunt sed
-        exercitationem. Cumque ad eum molestiae qui non aut. Laudantium culpa
-        dolor ipsa quaerat ipsum blanditiis quia dignissimos alias. Dolor
-        impedit praesentium optio fugiat excepturi quas quis et. Ut id eveniet
-        quisquam vitae earum non aperiam culpa. Vitae beatae et nisi ducimus qui
-        vel aut. Qui earum quidem assumenda est dolorem animi. Dolores
-        cupiditate corporis ut nemo et quis autem ipsa. Et quo voluptatibus qui
-        perspiciatis qui dolores voluptas minus.
-      </p>
-      <h1>Our Mission Statement</h1>
-      <p>
-        Rem architecto libero magni sint non nihil. Animi saepe consequatur
-        dolores impedit enim rerum molestiae perferendis at. Sunt quis
-        repudiandae vero aut. Eos qui beatae qui quaerat est. Et facere est eos
-        officiis adipisci amet magnam.
-      </p>
-      <h1>Visit Us</h1>
-      <p>
-        If you're in the San Diego area, you can see for yourself what makes us
-        different by visiting our physical nursery, where you can pick out
-        plants yourself and find other specimens not show on the site. Normally,
-        we are open to the public, but for the time being we are only taking
-        people who have appointments to limit the amount of visitors.
-      </p>
-      <Link to="/contact">
-        <button>Contact Us</button>
-      </Link>
-    </main>
+    <div>
+      <div style={bannerStyle}>
+        <div style={heroText}>
+          <h1>About Us</h1>
+        </div>
+      </div>
+      <main style={mainStyle}>
+        <div
+          style={{
+            ...sectionStyle,
+            backgroundColor: "#f6c7c7",
+            marginBottom: 80,
+            backgroundImage: `url(${storyImg})`,
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "#f0f8ff95",
+              padding: 10
+            }}
+          >
+            <h1 style={titleStyle}>Our Story</h1>
+            <hr style={breakStyle}></hr>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <p style={{ ...textStyle, width: "50%", padding: 30 }}>
+                Mollitia illo voluptatem tempore voluptatem. Molestiae dolorem
+                nostrum cum. Ut unde explicabo totam id consequuntur quae non.
+                Est sit sunt sed exercitationem. Cumque ad eum molestiae qui non
+                aut. Laudantium culpa dolor ipsa quaerat ipsum blanditiis quia
+                dignissimos alias. Dolor impedit praesentium optio fugiat
+                excepturi quas quis et. Ut id eveniet quisquam vitae earum non
+                aperiam culpa. Vitae beatae et nisi ducimus qui vel aut. Qui
+                earum quidem assumenda est dolorem animi. Dolores cupiditate
+                corporis ut nemo et quis autem ipsa. Et quo voluptatibus qui
+                perspiciatis qui dolores voluptas minus.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            ...sectionStyle,
+            backgroundColor: "#f6c7c7",
+            marginBottom: 80,
+            backgroundImage: `url(${missionImg})`,
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "#f0f8ff95",
+              padding: 20
+            }}
+          >
+            <h1 style={{textAlign: 'center'}}>Our Mission Statement</h1>
+            <hr style={breakStyle}></hr>
+            <p style={textStyle}>
+              Rem architecto libero magni sint non nihil. Animi saepe
+              consequatur dolores impedit enim rerum molestiae perferendis at.
+              Sunt quis repudiandae vero aut. Eos qui beatae qui quaerat est. Et
+              facere est eos officiis adipisci amet magnam.
+            </p>
+          </div>
+        </div>
+        <div style={{ backgroundColor: "#507dbc" }}>
+          <div style={{ ...sectionStyle, display: 'flex', flexDirection: 'column' }}>
+            <h1 style={{textAlign:'center'}}>Visit Us</h1>
+            <hr style={breakStyle}></hr>
+            <p style={textStyle}>
+              If you're in the San Diego area, you can see for yourself what
+              makes us different. Visit our nursery, where you can pick out
+              plants yourself and find other specimens we don't offer on the
+              site. Normally, we are open to the public, but for the safety of
+              our workers and visitors, we are only taking people who have
+              appointments to limit contact until further notice.
+            </p>
+            <Link to="/contact" style={{alignSelf:'center', margin: 10}}>
+              <button style={{}} className='contactBtn'>Contact Us</button>
+            </Link>
+          </div>
+        </div>
+      </main>
+      <RecommendedSection itemArr={[]}/>
+    </div>
   );
 };
 
