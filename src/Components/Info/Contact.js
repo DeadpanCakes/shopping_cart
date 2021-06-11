39feade825cd8f8369af6d41ade06acf7ba3fae3
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-06-10 18:20:36 -0700

    increased size of font in Contact to increase readability

--- src/Components/Info/Contact.js
+++ src/Components/Info/Contact.js
@@ -2,7 +2,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
 import InfoHero from "./InfoHero";
 import contactHero from "../../img/contactBannerFM.jpg";
-import nurseryImg from "../../img/nursery2.jpg";
+import nurseryImg from "../../img/nursery3.jpg";
+import nursery2Img from "../../img/nursery2.jpg";
 import SocialMedia from "../SocialMedia";
 import ContactForm from "./ContactForm";
 import RecommendedSection from '../RecommendedSection';
@@ -40,7 +41,7 @@ const Contact = () => {
             <h1>Send Us A Message</h1>
             <div style={{ display: "flex", justifyContent: "flex-start" }}>
               <ContactForm />
-              <p style={{ flex: "1", textAlign: "center", padding: 10 }}>
+              <p style={{ flex: "1", textAlign: "center", padding: 10, fontSize: '1.3em' }}>
                 Got questions or feedback? Or maybe you just wanna say thanks?
                 If you want a response, remember to leave your email. If you
                 need quick answers, be sure to check our FAQ.
@@ -61,9 +62,9 @@ const Contact = () => {
               <h1>Follow Us</h1>
               <SocialMedia size="4x" isDark={false} />
             </div>
-            <p>
+            <p style={{fontSize: '1.3em'}}>
               Keep yourself on updated on new, rare, or fast-selling stock!
-              Participate in exclusive deals and contests!
+              Participate in exclusive deals and giveaways!
             </p>
           </div>
         </section>
@@ -77,7 +78,7 @@ const Contact = () => {
             maxHeight: 300,
           }}
         >
-          <img alt="Our nursery" src={nurseryImg} style={{ maxWidth: 900 }} />
+          <img alt="Our nursery" src={nursery2Img} style={{ maxWidth: 900 }} />
           <h2
             style={{
               position: "absolute",
@@ -91,11 +92,11 @@ const Contact = () => {
             See What Makes Us Different For Yourself
           </h2>
         </div>
-        <section style={{ ...sectionStyle, alignSelf: "end", display: "flex" }}>
+        <section style={{ ...sectionStyle, alignSelf: "end", display: "flex", backgroundImage: `url(${nurseryImg})` }}>
           <div style={{ backgroundColor: "#f0f8ff95", padding: 15 }}>
             <div style={{ flex: 2 }}>
               <h1>Visit Us</h1>
-              <p>
+              <p style={{fontSize: '1.3em'}}>
                 You can visit us and pick out your own plants or find specimens
                 not listed on the site. Just give us a ring to set up an
                 appointment!
@@ -110,9 +111,9 @@ const Contact = () => {
               }}
             >
               <FontAwesomeIcon icon={faPhone}></FontAwesomeIcon>
-              <p>+1 (883) 841-2369</p>
+              <p style={{fontSize: '1.3em'}}>+1 (883) 841-2369</p>
               <FontAwesomeIcon icon={faMapMarkedAlt}></FontAwesomeIcon>
-              <p>475 Konopelski Harbors, San Diego, Ca 94516</p>
+              <p style={{fontSize: '1.3em'}}>475 Konopelski Harbors, San Diego, Ca 94516</p>
             </div>
           </div>
         </section>
