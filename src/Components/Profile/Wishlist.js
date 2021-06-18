d79bd57d06f251dc112dffb6411aa6a4f6a5bb48
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-06-17 18:57:25 -0700

    added a remove button to Wishlist

--- src/Components/Profile/Wishlist.js
+++ src/Components/Profile/Wishlist.js
@@ -4,11 +4,23 @@ const Wishlist = (props) => {
   return (
     <UserConsumer>
       {(user) => {
+        const { loggedUser, editUser } = user;
         return (
           <div>
             {user.loggedUser ? (
               user.loggedUser.wishList.map((item) => {
-                return <p key={item.name}>{item.name}</p>;
+                return (
+                  <li key={item.name}>
+                    <p>{item.name}</p>;
+                    <button
+                      onClick={() =>
+                        editUser(loggedUser.id, "removeWish", item)
+                      }
+                    >
+                      Remove
+                    </button>
+                  </li>
+                );
               })
             ) : (
               <p>Go log in</p>
