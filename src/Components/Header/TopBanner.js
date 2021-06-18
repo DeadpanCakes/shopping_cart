a27e3b46283619622e9cb14cdb105b6b051e516c
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-06-17 19:49:33 -0700

    added a conditional to TopBanner to display different text when user is logged in

--- src/Components/Header/TopBanner.js
+++ src/Components/Header/TopBanner.js
@@ -1,15 +1,29 @@
 import { faSeedling } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { UserConsumer } from "../../UserContext";
 
 const TopBanner = () => {
   return (
-    <div style={{color: '#DDFFF7',backgroundColor: '#2A2B2A'}}>
-      <h3 style={{textAlign: 'center'}}>
-        <FontAwesomeIcon icon={faSeedling} />
-        {' '} Sign Up For Exclusive Deals! {' '}
-        <FontAwesomeIcon icon={faSeedling} />
-      </h3>
-    </div>
+    <UserConsumer>
+      {(user) => {
+        const { loggedUser } = user;
+        return loggedUser ? (
+          <div style={{ color: "#DDFFF7", backgroundColor: "#2A2B2A" }}>
+            <h3 style={{ textAlign: "center" }}>
+              <FontAwesomeIcon icon={faSeedling} /> Use Offer Code: GRDOPN For
+              20% Off! <FontAwesomeIcon icon={faSeedling} />
+            </h3>
+          </div>
+        ) : (
+          <div style={{ color: "#DDFFF7", backgroundColor: "#2A2B2A" }}>
+            <h3 style={{ textAlign: "center" }}>
+              <FontAwesomeIcon icon={faSeedling} /> Sign Up For Exclusive Deals!{" "}
+              <FontAwesomeIcon icon={faSeedling} />
+            </h3>
+          </div>
+        );
+      }}
+    </UserConsumer>
   );
 };
 
