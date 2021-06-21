1815efe479586dea7b92694f9da07524536a8585
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-06-21 11:25:36 -0700

    adjusted logic of Recommendations to only refresh when an item is added from Recommendations to cart, to prevent unwanted refreshes when adjusting values in cart

--- src/Components/RecommendedSection.js
+++ src/Components/RecommendedSection.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import ItemListing from "./ItemListing";
 import useStock from "../items/itemHooks/useStock";
 
@@ -6,31 +6,43 @@ const RecommendedSection = (props) => {
   const stock = useStock();
   const [recommendedItems, setRecommended] = useState([]);
   const { itemArr } = props;
+  const isMounted = useRef(false);
 
   useEffect(() => {
-    if (stock.length > 0) {
-      const generateRecommended = (arr, num) => {
-        //Added num and extra check to prevent infinite loop if the user would put all items into cart, as a stopgap
-        if (arr.length >= 5 || num >=50) {
-          return arr;
-        }
-        const newItem = stock[Math.floor(Math.random() * stock.length)];
-        //Push new item if it is not a repeat and the user has not already added the item to the cart or is currently browsing that item's page
-        if (
-          !(
-            arr.includes(newItem) ||
-            itemArr.some((item) => item.id === newItem.id)
-          )
-        ) {
-          arr.push(newItem);
-        }
-        return generateRecommended(arr, num+1);
-      };
+    const generateRecommended = (arr, num) => {
+      //Added num and extra check to prevent infinite loop if the user would put all items into cart, as a stopgap
+      if (arr.length >= 5 || num >= 50) {
+        return arr;
+      }
+      const newItem = stock[Math.floor(Math.random() * stock.length)];
+      //Push new item if it is not a repeat and the user has not already added the item to the cart or is currently browsing that item's page
+      if (
+        !(
+          arr.includes(newItem) ||
+          itemArr.some((item) => item.id === newItem.id)
+        )
+      ) {
+        arr.push(newItem);
+      }
+      return generateRecommended(arr, num + 1);
+    };
 
-      const recommendations = generateRecommended([], 0);
-      setRecommended(recommendations);
+    if (stock.length > 0) {
+      if (recommendedItems.length < 1) {
+        const recommendations = generateRecommended([], 0);
+        setRecommended(recommendations);
+      } else if (
+        itemArr.find((item) => {
+          return recommendedItems.find((recommendation) => {
+            return recommendation.id === item.id;
+          });
+        })
+      ) {
+        const recommendations = generateRecommended([], 0);
+        setRecommended(recommendations);
+      }
     }
-  }, [stock, itemArr]);
+  }, [stock, itemArr, recommendedItems]);
 
   return (
     <div className="recommendationsContainer">
