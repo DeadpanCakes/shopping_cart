03f34bd1b95604027bcec13c6e77ea4ac88ef7e7
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-06-30 15:45:27 -0700

    added logic to Wishlist to redirect user to log in if they are not alread

--- src/Components/Profile/Wishlist.js
+++ src/Components/Profile/Wishlist.js
@@ -1,33 +1,42 @@
 import { CartConsumer } from "../../CartContext";
 import { UserConsumer } from "../../UserContext";
 import WishListing from "./WishListing";
+import { useHistory } from "react-router-dom";
 
 const Wishlist = (props) => {
+  const history = useHistory();
   return (
     <CartConsumer>
       {(cart) => {
-        const {addItem} = cart;
+        const { addItem } = cart;
         return (
           <UserConsumer>
             {(user) => {
+              (() => {
+                if (!user.loggedUser) {
+                  history.push("/profile/log-in");
+                }
+              })();
               const { loggedUser, editUser } = user;
               return (
                 <div>
-                  {user.loggedUser ? (
-                    user.loggedUser.wishList.map((item) => {
-                      return (
-                        <WishListing
-                          item={item}
-                          removeWish={() =>
-                            editUser(loggedUser.id, "removeWish", item)
-                          }
-                          addToCart={() => addItem({...item, quantity: 1})}
-                        />
-                      );
-                    })
-                  ) : (
-                    <p>Go log in</p>
-                  )}
+                  <ul>
+                    {user.loggedUser ? (
+                      user.loggedUser.wishList.map((item) => {
+                        return (
+                          <WishListing
+                            item={item}
+                            removeWish={() =>
+                              editUser(loggedUser.id, "removeWish", item)
+                            }
+                            addToCart={() => addItem({ ...item, quantity: 1 })}
+                          />
+                        );
+                      })
+                    ) : (
+                      <p>Go log in</p>
+                    )}
+                  </ul>
                 </div>
               );
             }}
