7b7c3488c07bf33ca9f0e86201c2fc4362d1ce74
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-07-01 10:31:58 -0700

    changed buttons in wishlist to render icons instead of text

--- src/Components/Profile/WishListing.js
+++ src/Components/Profile/WishListing.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt, faCartPlus } from "@fortawesome/free-solid-svg-icons";
 
 const WishListing = (props) => {
   const { item, removeWish, addToCart } = props;
@@ -7,25 +9,37 @@ const WishListing = (props) => {
   const listingStyle = {
     display: "flex",
     justifyContent: "space-between",
-    margin: "20px 0"
+    margin: "20px 0",
   };
   const imgStyle = {
     maxHeight: 100,
   };
   return (
     <li style={listingStyle} className="wishListing">
-      <Link to={`/shop/item/${id}`} style={{display :'flex', flex: 1}} className='itemLink'>
+      <Link
+        to={`/shop/item/${id}`}
+        style={{ display: "flex", flex: 1 }}
+        className="itemLink"
+      >
         <img src={imgSrc} alt={`A ${name}`} style={imgStyle}></img>
         <h2>{name}</h2>
       </Link>
-      <p style={{
-        fontSize: '1.3em',
-        flexShrink: 1,
-        margin: '10px 10px 0 0'
-      }}>${price}</p>
+      <p
+        style={{
+          fontSize: "1.3em",
+          flexShrink: 1,
+          margin: "10px 10px 0 0",
+        }}
+      >
+        ${price}
+      </p>
       <div style={{ display: "flex", flexDirection: "column" }}>
-        <button onClick={removeWish}>Remove</button>
-        <button onClick={addToCart}>Add</button>
+        <button onClick={removeWish}>
+          <FontAwesomeIcon icon={faTrashAlt} />
+        </button>
+        <button onClick={addToCart}>
+          <FontAwesomeIcon icon={faCartPlus} />
+        </button>
       </div>
     </li>
   );
