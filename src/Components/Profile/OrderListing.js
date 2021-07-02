66d5a372473e38183f3198fc4b36edf6ce2f4961
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-07-02 13:28:53 -0700

    fine tuned styling of OrderListing

--- src/Components/Profile/OrderListing.js
+++ src/Components/Profile/OrderListing.js
@@ -1,6 +1,8 @@
 import { format } from "date-fns";
 import { Link } from "react-router-dom";
 import OrderImgPreview from "./OrderImgPreview";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const OrderListing = (props) => {
   const { order } = props;
@@ -9,14 +11,25 @@ const OrderListing = (props) => {
 
   const listingStyle = {
     display: "flex",
-    margin: 20
+    margin: 20,
   };
 
   return (
     <li style={listingStyle}>
       <OrderImgPreview items={order.items} />
-      <Link to={`/shop/orders/${order.id}`}>
-        <button onClick={() => console.log(order)}>{formattedDate}</button>
+      <div>
+        <p>order#{order.id}</p>
+        <h1>{formattedDate}</h1>
+      </div>
+      <h2 style={{ marginLeft: "auto" }}>{order.price.total}</h2>
+      <Link
+        to={`/shop/orders/${order.id}`}
+        style={{ alignSelf: "flex-end", marginBottom: 30 }}
+      >
+        <button onClick={() => console.log(order)}>
+          See Details {" "}
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
       </Link>
     </li>
   );
