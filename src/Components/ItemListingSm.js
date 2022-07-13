49ac6fda469da611afd8837c84849853ac34e500
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-13 15:52:55 -0700

    added styles

--- src/Components/ItemListingSm.js
+++ src/Components/ItemListingSm.js
@@ -10,17 +10,33 @@ const CheckoutListing = (props) => {
         margin: "20px",
       }}
     >
-      <div style={{display: 'flex', position: 'relative', maxHeight: '70px', maxWidth: 70}}>
+      <div
+        style={{
+          display: "flex",
+          position: "relative",
+          maxHeight: "70px",
+          maxWidth: 70,
+        }}
+      >
         <img
           src={item.imgSrc}
           alt={`A(n) ${item.name}`}
           style={{ borderRadius: "5px" }}
         ></img>
-        <h3 style={{position: 'absolute', right: '-10%', top: '-10%', backgroundColor: 'pink', padding: '3px 6px', borderRadius: 100,}}>{item.quantity}</h3>
+        <h3
+          style={{
+            position: "absolute",
+            right: "-10%",
+            top: "-10%",
+            backgroundColor: "pink",
+            padding: "3px 6px",
+            borderRadius: 100,
+          }}
+        >
+          {item.quantity}
+        </h3>
       </div>
-      <h3
-        style={{ alignSelf: "center", marginLeft: "10px" }}
-      >{item.name}</h3>
+      <h3 style={{ alignSelf: "center", marginLeft: "10px" }}>{item.name}</h3>
       <h2
         style={{ fontSize: "2.5em", alignSelf: "flex-end", marginLeft: "auto" }}
       >{`$${item.quantity * item.price}`}</h2>
